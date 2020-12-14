504c22ba1421b570e59179062ca9bacd141f8d2a
woogeonsik1 <68350491+woogeonsik1@users.noreply.github.com>
2020-12-14 23:34:27 +0900

    setup

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
 
-const PORT = 5000;
+const PORT = 6000;
 const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
 
-const handleListening = () =>
-  console.log(`✅ Server runniung: http://localhost:${PORT}`);
+function handleListening() {
+  return console.log(`✅ Server runniung: http://localhost:${PORT}`);
+}
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
